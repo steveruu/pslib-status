c64bf80af10df63c2fd077f0c47309601dde6645
stepan <steveruu@stvr.cz>
2024-09-26 10:44:07 +0200

    client side fix

--- components/Status.tsx
+++ components/Status.tsx
@@ -1,7 +1,4 @@
 "use client";
-
-import { useState, useEffect } from "react";
-
 const Status = ({ ...props }: { name: string; status: string }) => {
     return (
         <div className="flex justify-between w-[250px] bg-slate-200 py-2 px-4 rounded-xl">
