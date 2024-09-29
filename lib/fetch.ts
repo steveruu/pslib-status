7849449d6c4d14f925a7af6ac5df5101b2457c05
stepan <steveruu@stvr.cz>
2024-09-29 21:17:37 +0200

    fix nefixoval, fix z vany #2

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,16 +1,26 @@
 "use server";
 
-export async function ping(url: string, timeout = 5000): Promise<boolean> {
+export async function ping(url: string): Promise<boolean> {
+    const timeout = 10000; // 10 seconds
+
     const controller = new AbortController();
     const timeoutId = setTimeout(() => controller.abort(), timeout);
 
-    try {
-        await fetch(url, { mode: "cors", cache: "no-cache", signal: controller.signal });
-        clearTimeout(timeoutId);
-        return true;
-    } catch (error) {
-        clearTimeout(timeoutId);
-        console.error(`Error pinging ${url}:`, error);
-        return false;
-    }
+    const fetchPromise = fetch(url, { mode: "cors", cache: "no-cache", signal: controller.signal })
+        .then(() => true)
+        .catch((error) => {
+            console.error(`Error pinging ${url}:`, error);
+            return false;
+        });
+
+    const timeoutPromise = new Promise<boolean>((resolve) => {
+        setTimeout(() => {
+            console.error(`Timeout error pinging ${url}`);
+            resolve(false);
+        }, timeout);
+    });
+
+    const result = await Promise.race([fetchPromise, timeoutPromise]);
+    clearTimeout(timeoutId);
+    return result;
 }
